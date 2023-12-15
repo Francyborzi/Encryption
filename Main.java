c9e0bc211b8192f961160870b593faf7e016d693
Francyborzi <fborzuola@gmail.com>
2023-12-15 14:54:19 +0000

    Modifica del 15/12/2023 FIX Exception

--- Main.java
+++ Main.java
@@ -1,21 +1,38 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    //ciao, questa è una prova
-    System.out.println("Cifriamo il nostro testo e trasformiamolo in un testo segreto!");
-    Matrice m = new Matrice("PrOVa");
+    // ciao, questa è una prova
+    System.out.println("Cifriamo il nostro testo e trasformiamolo in un testo segreto! \n");
+    Matrice m = new Matrice("PROVA");
     Vigenere v = new Vigenere(0, 12, 0, 12, m);
-    Vigenere v1 = new Vigenere(13, 25, 13, 25, m);
+    Vigenere v1 = new Vigenere(12, 26, 0, 12, m);
+    Vigenere v2 = new Vigenere(0, 12, 12, 26, m);
+    Vigenere v3 = new Vigenere(12, 26, 12, 26, m);
     Thread t = new Thread(v);
     Thread t1 = new Thread(v1);
+    Thread t2 = new Thread(v2);
+    Thread t3 = new Thread(v3);
     t.start();
     t1.start();
+    t2.start();
+    t3.start();
     try {
       t.join();
       t1.join();
+      t2.join();
+      t3.join();
     } catch (InterruptedException e) {
       System.err.println("Errore nel metodo join");
     }
 
     m.stampa();
+    
+    Scanner input = new Scanner(System.in);
+    System.out.println("\n Inserisci il testo in chiaro: ");
+    String testo_chiaro = input.nextLine();
+    String testo_cifrato = m.cifra(testo_chiaro);
+    System.out.println("\n");
+    System.out.println(testo_cifrato);
   }
 }
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # TITOLO
 
-Questa è una prova per il funzionamento della sincronizzazione del codice tra GitHub e Replit
+1) creare quattro Thread che popolano i quattro quadranti
+2) popolare la matrice di Vigenere con i quattro quadranti
+3) chiedere all'utente di inserire una chiave come input
+4) chiedere all'utente di inserire una frase da cifrare come input
+5) cifrare la frase e stampare il messaggio cifrato
